23eae87ad5b9445e118ecf1f7676739223b3ae4e
tyler97 <tdrover97@gmail.com>
2019-03-24 10:28:19 -0700

    Display search

--- flask/app/form.py
+++ flask/app/form.py
@@ -5,6 +5,6 @@ from wtforms.validators import DataRequired
 
 class SearchForm(FlaskForm):
     dataset = StringField('Dataset', validators=[DataRequired()])
-    startdate = DateField('Start date')
-    enddate = DateField('End date')
+    startdate = DateField('Start date', format='%Y-%m-%d', validators=[DataRequired()])
+    enddate = DateField('End date', format='%Y-%m-%d', validators=[DataRequired()])
     submit = SubmitField('Search')
--- flask/app/routes.py
+++ flask/app/routes.py
@@ -1,14 +1,17 @@
-from flask import render_template
+from flask import flash, render_template, redirect, request, url_for
 
 from app import app
 from app.form import SearchForm
 
 @app.route('/')
-@app.route('/index')
+@app.route('/home')
 def home():
     return render_template('home.html', title='Home')
 
-@app.route('/search')
+@app.route('/search', methods=['GET', 'POST'])
 def search():
     form = SearchForm()
+    if request.method == 'POST':
+        flash('Searching  dataset {} between {} and {}'.format(form.dataset.data, form.startdate.data, form.enddate.data))
+        return redirect(url_for('home'))
     return render_template('search.html', title='Search',form=form)
