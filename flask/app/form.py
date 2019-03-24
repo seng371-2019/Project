e655ac38c8750efb407c947f67b209a28337a01f
tyler97 <tdrover97@gmail.com>
2019-03-23 20:08:06 -0700

    Added feilds to search

--- flask/app/form.py
+++ flask/app/form.py
@@ -1,8 +1,10 @@
 
 from flask_wtf import FlaskForm
-from wtforms import StringField, BooleanField, SubmitField
+from wtforms import StringField, BooleanField, SubmitField, DateField
 from wtforms.validators import DataRequired
 
 class SearchForm(FlaskForm):
     dataset = StringField('Dataset', validators=[DataRequired()])
+    startdate = DateField('Start date')
+    enddate = DateField('End date')
     submit = SubmitField('Search')
