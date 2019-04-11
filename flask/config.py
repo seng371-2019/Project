a1f54f6f6563367e704c8d0eb6c85c506c0f0328
TylerDrover <tdrover97@gmail.com>
2019-04-10 22:19:30 -0700

    changed the config file

--- flask/config.py
+++ flask/config.py
@@ -1,4 +1,7 @@
 import os
 
 class Config(object):
+    PROJECT_ID = 'Project2STAC'
+    DATA_BACKEND = 'mongodb'
+    MONGO_URI = 'mongodb+srv://teamname:teamname@project2-stacdata-osuhm.gcp.mongodb.net/test?retryWrites=true'
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
